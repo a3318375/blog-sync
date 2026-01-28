f78db7c29cbf9fefa2253fc66704be21f1b94f6b
a3318375 <184608371@qq.com>
2026-01-28 17:51:36 +0800

    first commit

---
+++ src/main/java/com/yuxh/blog_sync/BlogSyncApplication.java
@@ -0,0 +1,13 @@
+package com.yuxh.blog_sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuxh/blog_sync/BlogSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuxh.blog_sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
